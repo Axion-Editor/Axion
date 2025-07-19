bce155d11f07963a067b18b15c0d26a89fc73ece
Vivek <itsvks19@gmail.com>
2025-07-19 15:26:21 +0530

    fix: update isNative function to use Capacitor for platform detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Capacitor } from "@capacitor/core";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, MemoryRouter, Route, Routes } from "react-router-dom";
 
-const isNative = () => {
-  return !!(window as any).Capacitor;
-};
+const isNative = () => Capacitor.getPlatform() !== "web";
 
 const queryClient = new QueryClient();
 
 const App = () => {
   const Router = isNative() ? MemoryRouter : BrowserRouter;
+
+  console.log(isNative() ? "Native" : "Browser");
 
   return (
     <QueryClientProvider client={queryClient}>
